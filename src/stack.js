75e5d4953c89cda055c3d5aed6ff430b776b9cec
Sayed Hassan <sayedhassanalavi0@gmail.com>
2023-05-06 17:23:14 +0200

    Chackar in förändringarna i src/stack.js (fick göra det senare)

---
+++ src/stack.js
@@ -0,0 +1,29 @@
+// Var
+const _ = require('underscore');
+
+let stack = [];
+
+// Lägger ett element överst i stacken
+exports.push = function (x) {
+    stack.push(x);
+};
+
+// Returnerar det översta elementet i stacken och tar bort det
+exports.pop = function () {
+    return stack.pop();
+}
+
+// Returnerar det översta elementet i stacken
+exports.peek = function () {
+    return _.last(stack);
+}
+
+
+
+//exports.peek = function () {
+    //return stack[0]; // Det här är medvetet felaktigt
+
+    // return _.last(stack); // Det här är rätt
+
+    //return stack[stack.length - 1];
+//};
